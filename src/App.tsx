3b7159eca18813ee1be36cbbb8f1bf1d4a452db7
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-31 18:27:04 +0500

    started

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,58 @@
+import Home from "./pages/home/Home"
+import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom"
+import Users from "./pages/users/Users"
+import Products from "./pages/products/Products"
+import NavBar from "./components/navbar/NavBar"
+import Menu from "./components/menu/Menu"
+import Footer from "./components/footer/Footer"
+import Login from "./pages/login/Login"
+import "./styles/global.scss"
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+  const Layout = () => {
+    return (
+      <div className="main">
+        <NavBar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users />
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />
+    }
+  ]);
+
+  return <RouterProvider router={router} />
 }
 
 export default App
